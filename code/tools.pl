594e910aef77b3c4ae15789cc1eab3a38dff2121
jfcloutier <jean.f.cloutier@gmail.com>
2026-06-08 09:20:21 -0400

    Added replicate/3 to tools

---
+++ code/tools.pl
@@ -0,0 +1,13 @@
+/*
+General utilities
+*/
+
+:- module(tools, [replicate/3]).
+
+%! replicate(+Item, +N:int, -Items:list) is det
+replicate(_, 0, []).
+replicate(Item, 1, [Item]).
+replicate(Item, N, [Item | Rest]) :-
+    N1 is N - 1,
+    replicate(Item, N1, Rest).
+
